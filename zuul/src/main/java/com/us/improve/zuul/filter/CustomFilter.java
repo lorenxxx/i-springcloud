be46b6554d9fad914bb8c3999f056470561925d4
liangliang <liangliang@icourt.cc>
2019-01-04 20:51:36 +0800

    feat: zuul 过滤完成

---
+++ zuul/src/main/java/com/us/improve/zuul/filter/CustomFilter.java
@@ -0,0 +1,54 @@
+package com.us.improve.zuul.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @ClassName CustomFilter
+ * @Desciption TODO
+ * @Author loren
+ * @Date 2019/1/4 8:39 PM
+ * @Version 1.0
+ **/
+@Component
+public class CustomFilter extends ZuulFilter {
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        Object token = request.getHeader("token");
+        if (token == null) {
+            ctx.setSendZuulResponse(false);
+            ctx.setResponseStatusCode(401);
+            try {
+                ctx.getResponse().getWriter().write("token is null");
+            } catch (Exception e) {
+
+            }
+            return null;
+        }
+
+        return null;
+    }
+
+}
