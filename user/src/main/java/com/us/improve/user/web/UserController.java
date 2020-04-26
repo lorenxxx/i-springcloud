cf4fa6eec957ff92c0810f52dba069881af88bc3
Loren <liangliangpro@outlook.com>
2020-04-26 18:42:47 +0800

    feat: 注册成功

--- user/src/main/java/com/us/improve/user/web/UserController.java
+++ user/src/main/java/com/us/improve/user/web/UserController.java
@@ -19,12 +19,15 @@ public class UserController {
     @Value("${server.port}")
     private String port;
 
+    @Value("${foo}")
+    private String foo;
+
     @Value("${maxim}")
     private String maxim;
 
     @GetMapping
     public String getUserById() {
-        return "Loren, " + maxim + ": " + port;
+        return foo + ", " + maxim + ": " + port;
     }
 
 }
