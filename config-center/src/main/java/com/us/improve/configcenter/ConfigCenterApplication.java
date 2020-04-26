7a52753ff71b0cb7aeee8ca403b117d941e76bde
Loren <liangliangpro@outlook.com>
2020-04-26 17:00:39 +0800

    feat: 注册成功

---
+++ config-center/src/main/java/com/us/improve/configcenter/ConfigCenterApplication.java
@@ -0,0 +1,16 @@
+package com.us.improve.configcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigCenterApplication.class, args);
+	}
+
+}
+
---
+++ gateway/src/test/java/com/us/improve/gateway/GatewayApplicationTest.java
@@ -0,0 +1,23 @@
+package com.us.improve.gateway;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @ClassName GatewayApplicationTest
+ * @Desciption TODO
+ * @Author loren
+ * @Date 2020/4/26 4:34 PM
+ * @Version 1.0
+ **/
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GatewayApplicationTest {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
